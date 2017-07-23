51703cdf68eb38fcabf5101745536b86ae32fccf
vagrant <vagrant@vagrant.vm>
2017-07-23 21:27:52 +0000

    Added homepage and top melons page

---
+++ server.py
@@ -0,0 +1,75 @@
+from flask import Flask, redirect, request, render_template, session
+from flask_debugtoolbar import DebugToolbarExtension
+from jinja2 import StrictUndefined
+
+
+app = Flask(__name__)
+app.jinja_env.undefined = StrictUndefined
+app.jinja_env.auto_reload = True
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+# Getting our list of MOST LOVED MELONS
+MOST_LOVED_MELONS = {
+    'cren': {
+        'img': 'http://www.rareseeds.com/assets/1/14/DimRegular/crenshaw.jpg',
+        'name': 'Crenshaw',
+        'num_loves': 584,
+    },
+    'jubi': {
+        'img': 'http://www.rareseeds.com/assets/1/14/DimThumbnail/Jubilee-Watermelon-web.jpg',
+        'name': 'Jubilee Watermelon',
+        'num_loves': 601,
+    },
+    'sugb': {
+        'img': 'http://www.rareseeds.com/assets/1/14/DimThumbnail/Sugar-Baby-Watermelon-web.jpg',
+        'name': 'Sugar Baby WatermemMlon',
+        'num_loves': 587,
+    },
+    'texb': {
+        'img': 'http://www.rareseeds.com/assets/1/14/DimThumbnail/Texas-Golden-2-Watermelon-web.jpg',
+        'name': 'Texas Golden Watermelon',
+        'num_loves': 598,
+    },
+}
+
+# YOUR ROUTES GO HERE
+@app.route("/")
+def index():
+    """Return homepage."""
+
+    if session["user_name"]:
+        return redirect("/top-melons")
+
+    return render_template("homepage.html")
+
+
+@app.route("/get-name", methods=["GET"])
+def get_name():
+    """Gets user's name and adds to session."""
+
+    name = request.args.get("my_name")
+    session["user_name"] = name
+    return redirect("/top-melons")
+
+
+@app.route("/top-melons")
+def show_top_melons():
+    """Displays top melons."""
+
+    if session["user_name"]:
+        return render_template("top-melons.html", melon_dict=MOST_LOVED_MELONS)
+    else:
+        return redirect("/")
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    # Use the DebugToolbar
+    # DebugToolbarExtension(app)
+
+    app.run(host="0.0.0.0")
